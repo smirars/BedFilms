57ed8524fff1b92204f4fdaed09a859cf355fca5
Arsonist <adsmi888@gmail.com>
2024-10-22 20:56:07 +0300

    add authorisation and registration logic

---
+++ backend/routes/auth.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+// Регистрация
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ msg: 'Пользователь уже существует' });
+    }
+
+    user = new User({ username, password });
+    await user.save();
+
+    const payload = { userId: user._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Ошибка сервера' });
+  }
+});
+
+// Авторизация
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Неверные учетные данные' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Неверные учетные данные' });
+    }
+
+    const payload = { userId: user._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Ошибка сервера' });
+  }
+});
+
+module.exports = router;
