145d2a1bede167fe1546f988fa35ae58372c8fff
Arsonist <adsmi888@gmail.com>
2024-10-24 17:18:10 +0300

    add routes

--- frontend/bedfilms/src/App.js
+++ frontend/bedfilms/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import AuthPage from './components/AuthPage'
+import WelcomePage from './WelcomePage'
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path="/" element={<AuthPage />} />
+              <Route path="/welcome" element={<WelcomePage />} />
           </Routes>
       </Router>
     );   
