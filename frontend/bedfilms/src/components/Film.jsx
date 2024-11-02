841dd2a2550a2c206809ad4299ceaa24e0206a6e
Arsonist <adsmi888@gmail.com>
2024-11-02 18:48:24 +0300

    add redirect to film page

--- frontend/bedfilms/src/components/Film.jsx
+++ frontend/bedfilms/src/components/Film.jsx
@@ -1,10 +1,10 @@
-// src/components/Film.jsx
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Film.css';
 
 const Film = ({ film }) => {
   const [isFavourite, setIsFavourite] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const favourites = JSON.parse(localStorage.getItem('favourites')) || [];
@@ -25,16 +25,18 @@ const Film = ({ film }) => {
     setIsFavourite(!isFavourite);
   };
 
+  const handleFilmClick = () => {
+    navigate(`/films/${film.id}`, { state: { film } });
+  };
+
   return (
-    <div className="film-card">
-      <Link to={`/catalog/${film.name}`}>
-        <img src={film.cover} alt={film.name} className="film-cover" />
-        <div className="film-info">
-          <h3>{film.name}</h3>
-          <p>{film.description}</p>
-        </div>
-      </Link>
-      <button className={`favourite-toggle ${isFavourite ? 'active' : ''}`} onClick={toggleFavourite}>
+    <div className="film-card" onClick={handleFilmClick}>
+      <img src={film.cover} alt={film.name} className="film-cover" />
+      <div className="film-info">
+        <h3>{film.name}</h3>
+        <p>{film.description}</p>
+      </div>
+      <button className={`favourite-toggle ${isFavourite ? 'active' : ''}`} onClick={(e) => { e.stopPropagation(); toggleFavourite(); }}>
         ❤️
       </button>
     </div>
