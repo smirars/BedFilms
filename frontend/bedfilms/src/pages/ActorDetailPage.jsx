09141b2a897349b22f6a7540f818aa3178b8f0e4
Arsonist <adsmi888@gmail.com>
2024-11-24 18:37:50 +0300

    add styles for actors detail page

--- frontend/bedfilms/src/pages/ActorDetailPage.jsx
+++ frontend/bedfilms/src/pages/ActorDetailPage.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Bale from '../images/Bale.jpg'
+import Cruise from '../images/tomcruise.jpg'
+import DiCaprio from '../images/dicaprio.jpg'
+import '../styles/ActorDetail.css';
 
 const actors = [
   {
     id: 1,
     name: "Кристиан Бейл",
-    photo: "actor1.jpg",
-    films: ["Фильм 1", "Фильм 2"],
-    biography: "Краткая биография...",
+    photo: Bale,
+    films: ["Криминальное чтиво", "Бешеные псы"],
+    biography: "Кри́стиан Чарльз Фи́лип Бейл (англ. Christian Charles Philip Bale; род. 30 января 1974, Хаверфордуэст, Пембрукшир, Уэльс, Великобритания) — британский актёр. Известен своей многосторонностью и физическими перевоплощениями для ролей. Имеет многочисленные награды, в том числе «Оскар», два «Золотых глобуса». В 2011 году журнал Time включил Бейла в список 100 самых влиятельных людей мира.",
   },
   {
     id: 2,
     name: "Леонардо ди Каприо",
-    photo: "actor2.jpg",
-    films: ["Фильм 2", "Фильм 3"],
-    biography: "Краткая биография...",
+    photo: DiCaprio,
+    films: ["Бешеные псы", "Интерстеллар"],
+    biography: "Леона́рдо Вильге́льм Ди Ка́прио (англ. Leonardo Wilhelm DiCaprio; род. 11 ноября 1974, Лос-Анджелес, США) — американский актёр и кинопродюсер. Лауреат многочисленных наград, включая премию «Оскар» (2016), BAFTA (2016), премию Гильдии киноактёров США (2016), три премии «Золотой глобус» (2005, 2014, 2016), а также «Серебряного медведя» (1997) Берлинского кинофестиваля.",
   },
   {
     id: 3,
     name: "Том Круз",
-    photo: "actor3.jpg",
-    films: ["Фильм 1", "Фильм 3"],
-    biography: "Краткая биография...",
+    photo: Cruise,
+    films: ["Криминальное чтиво", "Интерстеллар"],
+    biography: "Том Круз (англ. Tom Cruise, полное имя — То́мас Круз Мапотер IV (англ. Thomas Cruise Mapother IV); род. 3 июля 1962, Сиракьюс, Нью-Йорк, США) — американский актёр, кинорежиссёр, сценарист и кинопродюсер. Трёхкратный обладатель премии «Золотой глобус» (1990, 1997, 2000), двукратный обладатель премии «Сатурн» (2002, 2022) и четырёхкратный номинант на премию «Оскар».",
   },
 ];
 
@@ -36,13 +40,13 @@ const ActorDetailPage = () => {
     <div className="actor-detail-page">
       <Navbar />
       <div className="actor-detail-content">
-        <div className="actor-photo">
+        <div className="actor-face">
           <img src={actor.photo} alt={`${actor.name}`} />
         </div>
         <div className="actor-info">
           <h2>{actor.name}</h2>
           <h3>Фильмы:</h3>
-          <ul>
+          <ul className='films-list'>
             {actor.films.map((film, index) => (
               <li key={index}>
                 <Link to={`/films/${index + 1}`}>{film}</Link>
